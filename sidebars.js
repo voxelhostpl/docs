8ea6e51b71e9057097b6c83b13c0b07950aa3e8b
Astahiell <84834748+Astahiell@users.noreply.github.com>
2022-08-21 17:05:31 +0200

    PR fix + onlinemode -> start

--- sidebars.js
+++ sidebars.js
@@ -5,7 +5,7 @@ const sidebars = {
     {
       type: 'category',
       label: 'Poradniki',
-      items: ['tutorial/start', 'tutorial/onlinemode'],
+      items: ['tutorial/start',],
     },
   ],
 };
